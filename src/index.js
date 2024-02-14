300c967575c71443a842d6622701e3f640eee3c2
reim-weib-hr <hrrmanager985@gmail.com>
2024-02-14 14:33:29 -0500

    Initialize project using Create React App

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+// import Default from './App';
+// import { Transfer } from './Transfer';
+
+import Preloader from './Preloader/Preloader';
+
+
+const LazyLoadedComp = React.lazy(()=> import('./Transfer'));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Suspense fallback={<Preloader /> }>
+      <LazyLoadedComp />
+    </Suspense>
+  </React.StrictMode>
+);
+
+reportWebVitals();
